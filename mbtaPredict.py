71429c81b47260243936291df58f573c73e63c9b
shanek21 <shanegkelly21@gmail.com>
2015-03-29 21:17:11 -0400

    Code can now store weather data in .txt files in a folder. Also uploaded hourly weather data for all of 2014.

--- mbtaPredict.py
+++ mbtaPredict.py
@@ -14,4 +14,38 @@ def json(url):
 def weather_on(date):
 	return json(WUNDERGROUND_BASE_URL+ "/history_" + date + "/q/MA/Boston.json")
 
-print weather_on('20150328')
+def store_2014_data():
+	year = 2014
+	month = 1
+	day = 1
+	month31 = [1, 3, 5, 7, 8, 10, 12]
+	month30 = [4, 6, 9, 11]
+	month28 = [2]
+	number = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',
+			'16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+	for x in xrange(365):
+		for x in range(12):
+			day = 1
+			if month in month31:
+				for x in range(31):
+					date = str(year)+number[month]+number[day]
+					f = open(str(year)+'Data/'+str(year)+'-'+number[month]+'-'+number[day]+'.txt', 'w')
+					f.write(weather_on(date))
+					f.close()
+					day += 1
+			elif month in month30:
+				for x in range(30):
+					date = str(year)+number[month]+number[day]
+					f = open(str(year)+'Data/'+str(year)+'-'+number[month]+'-'+number[day]+'.txt', 'w')
+					f.write(weather_on(date))
+					f.close()
+					day += 1
+			elif month in month28:
+				for x in range(28):
+					date = str(year)+number[month]+number[day]
+					f = open(str(year)+'Data/'+str(year)+'-'+number[month]+'-'+number[day]+'.txt', 'w')
+					f.write(weather_on(date))
+					f.close()
+					day += 1
+			month += 1
+store_2014_data()
