dc0f8b5aaf7fa3ccd1e1f6a624788f51f2d1f6cb
William Lu <william.a.lu@gmail.com>
2015-04-11 23:27:09 -0400

    parsed data is now saved as txt file for future access

--- hubway_data_parsing.py
+++ hubway_data_parsing.py
@@ -4,18 +4,25 @@ import csv
 def load_trips(year):
 	"""
 	Input: Year in which you want to return stored Hubway data.
-	Output: List of dictionaries, where each dict contains the data for one Hubway trip
+	Output: Text file with a list of dictionaries, where each
+			dict contains the data for one Hubway trip
+	Text file name: YYYY_hubway_trips.txt
 	"""
 	data = []
-	file_name = "%s_hubway_trips.csv" % year
-	file_path = path.relpath("HubwayData/%s") % file_name
+	file_path = path.relpath("HubwayData/%s_hubway_trips.csv") % year
 	
+	#loads data from csv as a list of dictionaries
 	with open(file_path) as f:
 		reader = csv.DictReader(f)
 		for row in reader:
 			data.append(row)
 
-	return data
+	new_file_path = path.relpath("HubwayData/%s_hubway_trips.txt") % year
+
+	#saves list of dictionaries into txt file
+	f = open(new_file_path, 'a')
+	f.write(str(data))
+	f.close()
 
 if __name__ == "__main__":
-    print load_trips(2013)
+    load_trips(2013)
