931d9688284f30d63762a9daf20360d1d92f3eb4
shanek21 <shanegkelly21@gmail.com>
2015-05-05 15:03:46 -0400

    Added validation graph.

--- Tests/playground.py
+++ Tests/playground.py
@@ -1,4 +1,13 @@
-from datetime import date
+import matplotlib.pyplot as plt
 
-weekday = int(date(2015, 5, 5).weekday()<5)
-print weekday
+X = range(10)
+Y1 = range(10)
+Y2 = range(5,10) + range(5)
+
+first, = plt.plot(X, Y1, label='First')
+second, = plt.plot(X, Y2, label='Second')
+plt.title('This is a Title', fontsize=30)
+plt.xlabel('X Axis', fontsize=20)
+# plt.xlabel.set_fontsize(20)
+plt.legend(fontsize=20)
+plt.show()
